cd8c3f14dda95b87c4c5aaf808c2fc64cb7308e3
jason-s <jmsachs@gmail.com>
2015-03-12 19:41:13 -0700

    Update walk_subsets.py
    added example

--- walk_subsets.py
+++ walk_subsets.py
@@ -12,6 +12,63 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+example:
+
+    def example(items, skipitems):
+        l = []
+        for k,remove,remaining in walk_subsets(items):
+            for _ in xrange(remove):
+                l.pop()
+            l.append(k)
+            if l in skipitems:
+                del remaining[:]
+            print l
+            
+    example(range(4), ())
+    print ""
+    example(range(5), ([1], [0,2]))
+    
+output:
+
+    [0]
+    [0, 1]
+    [0, 1, 2]
+    [0, 1, 2, 3]
+    [0, 1, 3]
+    [0, 2]
+    [0, 2, 3]
+    [0, 3]
+    [1]
+    [1, 2]
+    [1, 2, 3]
+    [1, 3]
+    [2]
+    [2, 3]
+    [3]
+    
+    [0]
+    [0, 1]
+    [0, 1, 2]
+    [0, 1, 2, 3]
+    [0, 1, 2, 3, 4]
+    [0, 1, 2, 4]
+    [0, 1, 3]
+    [0, 1, 3, 4]
+    [0, 1, 4]
+    [0, 2]
+    [0, 3]
+    [0, 3, 4]
+    [0, 4]
+    [1]
+    [2]
+    [2, 3]
+    [2, 3, 4]
+    [2, 4]
+    [3]
+    [3, 4]
+    [4]
+
 """
 
 def walk_subsets(items):
